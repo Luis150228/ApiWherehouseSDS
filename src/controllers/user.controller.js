5ea433d7a625606443a9397a20b9fe232d56bae1
Rangel Diaz Luis Fernando <60003208+Luis150228@users.noreply.github.com>
2025-06-12 11:48:13 -0600

    Crear user.controller.js
    se crea primer controlador

---
+++ src/controllers/user.controller.js
@@ -0,0 +1,10 @@
+import sequelize from '../config/db.js';
+
+export const getUsers = async (req, res, next) => {
+  try {
+    const users = await sequelize.query('CALL stp_listarUsuarios()');
+    res.json(users);
+  } catch (error) {
+    next(error);
+  }
+};
