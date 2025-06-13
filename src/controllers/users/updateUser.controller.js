a2ba18d3c701f53a4d9e06a91908eaf756f02727
Rangel Diaz <luis.rangeldiaz@gmail.com>
2025-06-12 18:22:05 -0600

    Update: Store procedure names change

--- src/controllers/users/updateUser.controller.js
+++ src/controllers/users/updateUser.controller.js
@@ -1,20 +1,17 @@
 import sequelize from '../../config/db.js';
 
 export const updateUsers = async (req, res, next) => {
-    try {
-    const {num_user, nombre} = req.body;
-    const [updateUser] = await sequelize.query(
-        'CALL stp_updateUser(:num_user, :nombre)',
-        {
-            replacements:{num_user, nombre}
-        }
-    );
-    console.log(res)
-    res.json({
-        message: "Usuario Actualizado correctamente",
-        data: updateUser
-    })
-  } catch (error) {
-    next(error);
-  }
+	try {
+		const { num_user, nombre } = req.body;
+		const [updateUser] = await sequelize.query('CALL stp_usersUpdate(:num_user, :nombre)', {
+			replacements: { num_user, nombre },
+		});
+		console.log(res);
+		res.json({
+			message: 'Usuario Actualizado correctamente',
+			data: updateUser,
+		});
+	} catch (error) {
+		next(error);
+	}
 };
