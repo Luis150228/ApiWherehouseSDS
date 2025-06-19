7047eae44390667d2af8b0c767fb5551631517eb
LUIS FERNANDO RANGEL DIAZ <C356882@produban.com.mx>
2025-06-19 13:28:11 -0600

    Mostras informacion del usuario actual en base al token

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -4,11 +4,13 @@ import { updateUsers } from '../controllers/users/updateUser.controller.js';
 import validateHeaders from '../middleware/validateHeaders.js';
 import authenticate from '../middleware/authenticate.js';
 import { createUsers } from '../controllers/users/create.controller.js';
+import { getUserInfo } from '../controllers/users/getUserInfo.controller.js';
 
 const router = express.Router();
 
 router.get('/', authenticate, getUsers);
+router.get('/info', authenticate, getUserInfo);
 router.put('/', authenticate, updateUsers);
-router.post('/', validateHeaders, createUsers);
+router.post('/', authenticate, createUsers);
 
 export default router;
