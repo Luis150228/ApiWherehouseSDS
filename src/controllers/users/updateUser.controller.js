26ec1d62e85fad030ba9c6364bfcc7d27b0603a8
LUIS FERNANDO RANGEL DIAZ <C356882@produban.com.mx>
2025-06-26 10:24:16 -0600

    Update: Modificar el password y estatus de un usuario y se agrega uuid para el token

--- src/controllers/users/updateUser.controller.js
+++ src/controllers/users/updateUser.controller.js
@@ -2,14 +2,14 @@ import sequelize from '../../config/db.js';
 
 export const updateUsers = async (req, res, next) => {
 	try {
-		const { num_user, nombre } = req.body;
+		const { num_user, nombre, password, almacen, estatus } = req.body;
 		if (!num_user || !nombre) {
 			return res.status(400).json({
 				message: 'Faltan parámetros obligatorios: user, nombre son requeridos',
 			});
 		}
-		const [updateUser] = await sequelize.query('CALL stp_usersUpdate(:num_user, :nombre)', {
-			replacements: { num_user, nombre },
+		const [updateUser] = await sequelize.query('CALL stp_usersUpdate(:num_user, :nombre, :password, :almacen, :estatus)', {
+			replacements: { num_user, nombre, password, almacen, estatus },
 		});
 		console.log(res);
 		res.status(200).json({
--- src/utils/token.js
+++ src/utils/token.js
@@ -1,9 +1,10 @@
 import jwt from 'jsonwebtoken';
+import {v4 as uuidv4} from 'uuid'
 
 const SECRET_KEY = process.env.JWT_SECRET || 'clavePorDefecto';
 
 export const generateToken = (payload) => {
-  return jwt.sign(payload, SECRET_KEY, { expiresIn: '3h' });
+  return jwt.sign(payload, SECRET_KEY, { expiresIn: '3h', jwtid: uuidv4() });
 };
 
 export const verifyToken = (token) => {
